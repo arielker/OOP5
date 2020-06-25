277649b5fb43648fbd4c8d9074a29abe85a15bcb
arielker <62556723+arielker@users.noreply.github.com>
2020-06-25 20:33:26 +0300

    theoretically fimished part1 (?)

--- part1/MatrixOperations.h
+++ part1/MatrixOperations.h
@@ -3,6 +3,80 @@
 
 #include "Utilities.h"
 
+//AddLists:
+template<typename, typename...>
+struct AddLists {};
+
+template<typename L1, typename L2>
+struct AddLists<L1, L2>{
+    typedef typename PrependLists<Int<L1::head::value + L2::head::value>, AddLists<L1::next, L2::next>::result> result;
+    // TODO: Check if the matrix values type is promised to be Int or nah
+};
+
+template<>
+struct AddLists<List<>, List<>> {
+    typedef List<> result;
+};
+
+//ScalarMultiply:
+template<typename, typename...>
+struct ScalarMultiply {};
+
+template<typename L1, typename L2>
+struct ScalarMultiply<L1, L2>{
+    typedef typename Int<L1::head::value * L2::head::value) + (ScalarMultiply<L1::next, L2::next>::result::value)> result;
+};
+
+template<>
+struct ScalarMultiply<List<>, List<>>{
+    typedef Int<0> result;
+};
+
+template<typename, typename>
+struct VecMatMultiply {};
+
+template<typename V, typaname M>
+struct VecMatMultiply<V, M> {
+    typedef typename PrependList<ScalarMultiply<V, M::head>, VecMatMultiply<V, M::next>::result> result;
+}
+
+template<typename V>
+struct VecMatMultiply<V, List<>> {
+    typedef List<> result;
+}
+
+
+//Add:
+template<typename, typename...>
+struct Add {};
+
+template<typename M1, typename M2>
+struct Add<M1, M2> { //TODO: check if we need to assert validity of the input!
+    static_assert(M1::size == M2::size, "Matrix addition is not defined, rows are unequal");
+    static_assert(M1::head::size == M2::head::size, "Matrix addition is not defined, columns are unequal");
+    typedef typename PrependList<AddLists<M1::head, M2::head>::result, Add<M1::next, M2::next>::result> result;
+};
+
+template<>
+struct Add<List<>, List<>> {
+    typedef List<> result;
+};
+
+//Multiply:
+template<typename, typename>
+struct Multiply {};
+
+template<typename M1, typename M2>
+struct Multiply<M1, M2> {
+    static_assert(M1::head::size == M2::size, "Matrix Multiplication is not defined for these matrices");
+    //call some auxilary function
+    typename typedef PrependList<VecMatMultiply<M1::head, M2>::result, Multiply<M1::next, M2>::result> result;
+}
+
+template<M>
+struct Multiplt<List<>, M2> {
+    typename List<> result;
+}
 
 
 #endif
--- part1/Utilities.h
+++ part1/Utilities.h
@@ -2,15 +2,22 @@
 #define _UTILITIES_H
 
 //List:
-template<typename... TT>
-struct List;
+template<typename...>
+struct List {};
 
 template<typename T, typename... TT>
-struct List<T, TT...> { //TODO: check maybe List<T, TT> ?
+struct List<T, ...TT> { //TODO: check maybe List<T, TT> ?
     typedef T head;
     typedef List<TT...> next;
     constexpr static int size = 1 + sizeof...(TT);
 };
+
+// template<typename T>
+// struct List<T> {
+//     typedef T head;
+//     constexpr static int size = 1;
+
+// };
 
 template<>
 struct List<> {
@@ -18,41 +25,64 @@ struct List<> {
 };
 
 //PrependList:
-template<typename U, typename T, typename... TT>
-struct PrependList;
+template<typename, typename, typename...>
+struct PrependList {};
 
-
-template<typename U, typename T, typename... TT>
-struct PrependList<U, List<T, TT...> L>{
+template<typename U, typename T, typename... TT> // Possible bug - U isn't guaranteed to be the same type as T and TT
+struct PrependList<U, List<T, TT...>>{
     typedef List<U, T, TT...> list;
-}; //TODO: check empty list case!
+}; //TODO: check specialization for empty list case!
+
 
 //ListGet:
+template<int, typename...>
+struct ListGet {};
+
 template<int N, typename T, typename... TT>
-struct ListGet<N, List<T, TT...> L> {
-    typedef typename ListGet<N-1, L::next> value;
+struct ListGet<N, List<T, TT>> {
+    typedef typename ListGet<N-1, List<T, TT...>::next>::value value;
 };
 
-template<>
-struct ListGet<0, L>{
+template<typename T, typename... TT>
+struct ListGet<0, List<T, TT...>>{
     typedef T value;
 };
 
 //ListSet:
-template<int N, typename U, List<T, TT...> L>
-struct ListSet {
-    typedef typename ListSet<N-1, L::next> list;
+template<int, typename, typename, typename...>
+struct ListSet {};
+
+template<int N, typename U, typename T, typename... TT>
+struct ListSet<N, U, List<T, TT...>> {
+    // typedef typename ListSet<N-1, List<T,...TT>::next>::list list;
+    typedef typename PrependList<T, ListSet<N-1, U, List<T, ...TT>::next>::list list;
 };
 
-template<>
-struct ListSet<0, U, L>{
-    typedef PrependList<U, L::next>::list list;
+template<typename U, typename T, typename... TT>
+struct ListSet<0, U, List<T, TT...>>{
+    typedef PrependList<U, List<T, ...TT>::next>::list list;
 };
 
 //Int<N>
+template<int>
+struct Int {};
+
 template<int N>
 struct Int {
     constexpr static int value = N;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #endif
