66e5e06726ad6b57b9726908de3ae9580c694ba9
arielker <62556723+arielker@users.noreply.github.com>
2020-06-26 18:56:29 +0300

    added Stream.h

---
+++ part2/Stream.h
@@ -0,0 +1,15 @@
+#ifndef OOP5_STREAM_H
+#define OOP5_STREAM_H
+
+#include <functional>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+
+
+#endif //OOP5_STREAM_H
